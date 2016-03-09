ef05312884bacce1f0a37bcfae06b62d0f01aa75
Suriyadeepan Ram <suriyadeepan@acm.org>
2016-03-09 11:20:29 +0530

    Bounding box : ROI works pretty well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 
 #####
 ## tools
@@ -7,6 +7,25 @@ def initCap():
     return cv2.VideoCapture('data/cam131.avi')
 def gray(im):
     return cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+def diff_abs(prev,cframe,fframe):
+    abs_diff1 = cv2.absdiff(gray(prev),gray(fframe))
+    abs_diff2 = cv2.absdiff(gray(cframe),gray(fframe))
+    return cv2.bitwise_and(abs_diff1,abs_diff2)
+def diff_abs1(prev,cframe):
+    return cv2.absdiff(gray(prev),gray(cframe))
+def boundingBox(thresh,canvas):
+    # find bounding rectangle
+    ##
+    # get non-zero indices
+    nzx,nzy = np.nonzero(thresh)
+    if any(nzx) and any(nzy):
+        pt1 = (np.min(nzy),np.min(nzx))
+        pt2 = (np.max(nzy),np.max(nzx))
+        cv2.rectangle(canvas,pt1,pt2,(0,255,0),2)
+        return canvas,thresh[pt1[1]:pt2[1],pt1[0]:pt2[0]]
+    else:
+        return canvas,thresh
+   
 ####
 
 ###
@@ -25,15 +44,18 @@ while active:
     if active:
         # get difference image
         #   diff = cv2.subtract(gray(frame),prev)
-        abs_diff1 = cv2.absdiff(gray(prev),gray(cframe))
-        abs_diff2 = cv2.absdiff(gray(cframe),gray(fframe))
-        diff = cv2.bitwise_and(abs_diff1,abs_diff2)
+        #   diff = diff_abs(prev,cframe,fframe)
+        diff = diff_abs1(prev,cframe)
 
         # thresholding
-        _,thresh = cv2.threshold(abs_diff1,50,255,cv2.THRESH_BINARY)
+        _,thresh = cv2.threshold(diff,50,255,cv2.THRESH_BINARY)
+
+        bbox,roi = boundingBox(thresh,cframe.copy())
         
         cv2.imshow('Temporal Difference',diff)
         cv2.imshow('Threshold',thresh)
+        cv2.imshow('BoundingBox',bbox)
+        cv2.imshow('ROI',roi)
         cv2.waitKey(40)
 
         prev = cframe
